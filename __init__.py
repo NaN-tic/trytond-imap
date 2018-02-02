67d32f41d37a3c8c6f65f1482cff8130f0cf8d7e
?ngel ?lvarez <angel@nan-tic.com>
2018-02-02 11:10:16 +0100

    update to 4.7

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .imap import *
+from . import imap
 
 def register():
     Pool.register(
-        IMAPServer,
+        imap.IMAPServer,
         module='imap', type_='model')
